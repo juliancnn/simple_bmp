e333705fd00cc10ff754df7f432fca324f7976c5
dabratte <abrattediego@gmail.com>
2020-04-23 09:41:22 -0300

    add focus func

--- example.c
+++ example.c
@@ -3,21 +3,50 @@
 #include <inttypes.h>
 #include "simple_bmp.h"
 
-
 void fractal_generate (sbmp_raw_data **, int32_t, int32_t);
-
-int main(){
+void focus_center (sbmp_raw_data **, int32_t, int32_t);
+int main ()
+{
   sbmp_image test_img;
   sbmp_load_bmp ("base.bmp", &test_img);
-  printf("altura : %d\n", test_img.info.image_height);
-  printf("ancho : %d\n", test_img.info.image_width);
-  fractal_generate (test_img.data, test_img.info.image_height, test_img.info.image_width);
+  printf ("altura : %d\n", test_img.info.image_height);
+  printf ("ancho : %d\n", test_img.info.image_width);
+  focus_center (test_img.data, test_img.info.image_height, test_img.info.image_width);
   sbmp_save_bmp ("frac.bmp", &test_img);
 }
 
 /*
  * \@details http://www.albertostrumia.it/sites/default/files/Fractals/FractalGallery2/EW2.html
  */
+
+void focus_center (sbmp_raw_data **img_data, int32_t height, int32_t width)
+{
+  const int radio = width / 4 ;
+  const int centerx = abs (width / 2);
+  const int centery = abs (height / 2);
+
+  for (int y = 0; y < height; y++)
+    {
+      for (int x = 0; x < width; x++)
+        {
+          if ((centerx - x) * (centerx - x) + (centery - y) * (centery - y) <= radio * radio)
+            {
+              //img->img_pixels[y][x] = BMP_PIXEL (255,0,255);
+
+              img_data[y][x].red = (uint8_t)
+                  (img_data[y][x].red * 1.5 > 255 ? 255 : (uint8_t) (img_data[y][x].red * 1.5));
+
+              img_data[y][x].green = (uint8_t)
+                  (img_data[y][x].green * 1.5 > 255 ? 255 : (uint8_t) (img_data[y][x].green * 1.5));
+              img_data[y][x].blue = (uint8_t)
+                  (img_data[y][x].blue * 1.5 > 255 ? 255 : (uint8_t) (img_data[y][x].blue * 1.5));
+
+            }
+        }
+    }
+
+}
+
 void fractal_generate (sbmp_raw_data **img_data, int32_t height, int32_t width)
 {
   const double Radius = 0.8;
@@ -60,8 +89,8 @@ void fractal_generate (sbmp_raw_data **img_data, int32_t height, int32_t width)
 
               if ((x - Cx) * (x - Cx) + (y - Cy) * (y - Cy) < Radius)
                 {
-                  //printf ("%i\n", n);
                   w = n;
+
                   n = Num;
                 }
 
@@ -69,7 +98,15 @@ void fractal_generate (sbmp_raw_data **img_data, int32_t height, int32_t width)
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wconversion"
-          img_data[p][q] = (sbmp_raw_data) {(uint8_t)(w*1.5), 0, (uint8_t) (w - 1)};
+          img_data[p][q] = (sbmp_raw_data) {
+              //(uint8_t)((float) img->data[p][q].blue + (float) w/128),
+              ((w > 30) ? (uint8_t) (abs ((int32_t) ((float) img_data[p][q].blue * 1.2 - 46))) : (uint8_t) abs (
+                  (int32_t) img_data[p][q].blue + 10)),
+              ((w > 30) ? (uint8_t) (abs ((int32_t) ((float) img_data[p][q].green * 1.2 - 46))) : (uint8_t) abs (
+                  (int32_t) img_data[p][q].green + 10)),
+              ((w > 30) ? (uint8_t) (abs ((int32_t) ((float) img_data[p][q].red * 1.2 - 46))) : (uint8_t) abs (
+                  (int32_t) img_data[p][q].red + 10)),
+          };
 #pragma GCC diagnostic pop
         }
     }
